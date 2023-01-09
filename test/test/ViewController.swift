73bb46bfbdf15dff93d72e4a0463c42a1f4f0ca7
OLLO77 <petkooleg@gmail.com>
2023-01-09 17:30:07 +0000

    My first commit

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Oleg Petco on 09/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
